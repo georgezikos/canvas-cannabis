00d5b0def27d1a14b7344d3c347cca1767d2db42
georgezikos <george.zikos@icloud.com>
2021-07-20 01:05:17 -0400

    breaking out into modules

---
+++ src/dailySpecials.js
@@ -0,0 +1,31 @@
+// reset inline display flex for off days?
+// correct this code + opt for dynamic class vs. inline-styling
+
+const $mondaySpecials = $('#monday-specials');
+const $tuesdaySpecials = $('#tuesday-specials');
+const $wednesdaySpecials = $('#wednesday-specials');
+const $thursdaySpecials = $('#thursday-specials');
+const currentDate = new Date();
+const currentDay = currentDate.getDay();
+
+const showSpecials = () => {
+  if (currentDay === 0 || currentDay === 6 || currentDay === 5) {
+    console.log('Not today, freak!');
+  } else if (currentDay === 1) {
+    $mondaySpecials.css('display', 'flex');
+    console.log('Monday!');
+  } else if (currentDay === 2) {
+    $tuesdaySpecials.css('display', 'flex');
+    console.log('Tuesday!');
+  } else if (currentDay === 3) {
+    $wednesdaySpecials.css('display', 'flex');
+    console.log('Wednesday!');
+  } else if (currentDay === 4) {
+    $thursdaySpecials.css('display', 'flex');
+    console.log('Thursday!');
+  }
+};
+
+showSpecials();
+
+export default dailySpecials;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
-const canvasCannabis = {};
+import dailySpecials from './dailySpecials';
 
-canvasCannabis.init = () => {
-  canvasCannabis.mondaySpecials = $('#monday-specials');
-  canvasCannabis.tuesdaySpecials = $('#tuesday-specials');
-  canvasCannabis.wednesdaySpecials = $('#wednesday-specials');
-  canvasCannabis.thursdaySpecials = $('#thursday-specials');
-  canvasCannabis.currentDate = new Date();
-  // reset inline display flex for off days?
-  // correct this code + opt for dynamic class vs. inline-styling
-  canvasCannabis.showSpecials = () => {
-    if (
-      canvasCannabis.currentDate.getDay() === 0 ||
-      canvasCannabis.currentDate.getDay() === 6 ||
-      canvasCannabis.currentDate.getDay() === 5
-    ) {
-      console.log('Not today, freak!');
-    } else if (canvasCannabis.currentDate.getDay() === 1) {
-      canvasCannabis.mondaySpecials.css('display', 'flex');
-      console.log('Monday!');
-    } else if (canvasCannabis.currentDate.getDay() === 2) {
-      canvasCannabis.tuesdaySpecials.css('display', 'flex');
-      console.log('Tuesday!');
-    } else if (canvasCannabis.currentDate.getDay() === 3) {
-      canvasCannabis.wednesdaySpecials.css('display', 'flex');
-      console.log('Wednesday!');
-    } else if (canvasCannabis.currentDate.getDay() === 4) {
-      canvasCannabis.thursdaySpecials.css('display', 'flex');
-      console.log('Thursday!');
-    }
-  };
-  canvasCannabis.showSpecials();
+const init = () => {
+  dailySpecials();
 };
 
 $(function () {
-  canvasCannabis.init();
+  init();
 });
