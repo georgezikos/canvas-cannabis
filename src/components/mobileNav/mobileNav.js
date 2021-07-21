9c3f831849b0e92dc6d245c4a239c668b53b31bc
georgezikos <george.zikos@icloud.com>
2021-07-20 21:53:49 -0400

    Style changes to navbar on mobile

--- src/components/mobileNav/mobileNav.js
+++ src/components/mobileNav/mobileNav.js
@@ -1,34 +1,46 @@
 import './mobileNav.css';
 
 // DOM Selectors
+const $window = $(window);
 const $body = $('body');
 const $hamburger = $('.main-nav__hamburger');
 const $linksList = $('.main-nav__links-list');
-const $window = $(window);
+const $mainNav = $('nav.main-nav');
+const $navLogo = $('img.main-nav__logo');
+const $hamburgerBottom = $('div.hamburger__bottom');
+const $hamburgerTop = $('div.hamburger__top');
 
 // CSS Classes
 const activeMobileNav = 'main-nav__links-list--active';
 const disableScroll = 'active-nav';
+const mainNavActiveMobile = 'main-nav--active-mobile';
+const navLogoActiveMobile = 'main-nav__logo--active-mobile';
+const hamburgerBunsActiveMobile = 'hamburger--active';
 
 // Other
 const windowNavClose = 991;
 
 // Functions
 const navHandler = () => {
+  $mainNav.toggleClass(mainNavActiveMobile);
+  $navLogo.toggleClass(navLogoActiveMobile);
+  $hamburgerBottom.toggleClass(hamburgerBunsActiveMobile);
+  $hamburgerTop.toggleClass(hamburgerBunsActiveMobile);
   $linksList.toggleClass(activeMobileNav);
   $body.toggleClass(disableScroll);
 };
 
 const mobileNav = () => {
+  // Hamburger click handler
   $hamburger.on('click', () => {
     navHandler();
   });
+  // Window resize handler
   $window.on('resize', () => {
     if (
       $linksList.hasClass(activeMobileNav) &&
       $window.width() > windowNavClose
     ) {
-      console.log(window.innerWidth);
       navHandler();
     }
   });
