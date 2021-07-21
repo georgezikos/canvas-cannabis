78bfc3f6ba2e947cd3ae23d7de0aac064b97071a
georgezikos <george.zikos@icloud.com>
2021-07-21 17:48:52 -0400

    Alternative solution to mobile menu vertical scrolling

--- src/components/mobileNav/mobileNav.js
+++ src/components/mobileNav/mobileNav.js
@@ -2,31 +2,32 @@ import './mobileNav.css';
 
 // DOM Selectors
 const $window = $(window);
-const $body = $('body');
+// const $body = $('body');
+const $html = $('html');
 const $hamburger = $('.main-nav__hamburger');
 const $linksList = $('.main-nav__links-list');
-const $mainNav = $('nav.main-nav');
-const $navLogo = $('img.main-nav__logo');
-const $hamburgerBottom = $('div.hamburger__bottom');
-const $hamburgerTop = $('div.hamburger__top');
+// const $mainNav = $('nav.main-nav');
+// const $navLogo = $('img.main-nav__logo');
+// const $hamburgerBottom = $('div.hamburger__bottom');
+// const $hamburgerTop = $('div.hamburger__top');
 
 // CSS Classes
 const activeMobileNav = 'main-nav__links-list--active';
 const disableScroll = 'active-nav';
-const mainNavActiveMobile = 'main-nav--active-mobile';
-const navLogoActiveMobile = 'main-nav__logo--active-mobile';
-const hamburgerBunsActiveMobile = 'hamburger--active';
+// const mainNavActiveMobile = 'main-nav--active-mobile';
+// const navLogoActiveMobile = 'main-nav__logo--active-mobile';
+// const hamburgerBunsActiveMobile = 'hamburger--active';
 
 // Other
 const windowNavClose = 991;
 
 // Functions
 const navHandler = () => {
-  $mainNav.toggleClass(mainNavActiveMobile);
-  $navLogo.toggleClass(navLogoActiveMobile);
-  $body.toggleClass(disableScroll);
-  $hamburgerBottom.toggleClass(hamburgerBunsActiveMobile);
-  $hamburgerTop.toggleClass(hamburgerBunsActiveMobile);
+  // $mainNav.toggleClass(mainNavActiveMobile);
+  // $navLogo.toggleClass(navLogoActiveMobile);
+  $html.toggleClass(disableScroll);
+  // $hamburgerBottom.toggleClass(hamburgerBunsActiveMobile);
+  // $hamburgerTop.toggleClass(hamburgerBunsActiveMobile);
   $linksList.toggleClass(activeMobileNav);
 };
 
