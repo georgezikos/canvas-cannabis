7efe5da87a79d39ecb70705821442cb1c5f51431
georgezikos <george.zikos@icloud.com>
2021-07-22 17:26:48 -0400

    Nothing new

--- src/components/allDeviceNav/allDeviceNav.js
+++ src/components/allDeviceNav/allDeviceNav.js
@@ -96,7 +96,7 @@ const menuCloseHandler = () => {
   // Clicking away from element
   $document.on('click', (e) => {
     // $dropdownSubMenu.hasClass(activeSubMenu) && $(e.target).not($mainNav)
-    if ($(e.currentTarget).not($mainNav)) {
+    if (e.closest($mainNav)) {
       // $dropdownSubMenu.removeClass(activeSubMenu);
       // $dropdownIcon.removeClass(activeDropdownIcon);
       console.log('Clicking away');
