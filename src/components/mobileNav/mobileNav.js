97684fcf6dab486b8c79dfff7f5031b3cc355b96
georgezikos <george.zikos@icloud.com>
2021-07-21 23:34:00 -0400

    Again and again

--- src/components/mobileNav/mobileNav.js
+++ src/components/mobileNav/mobileNav.js
@@ -67,7 +67,7 @@ const mobileNav = () => {
   // Submenu handler
   $dropdownLink.on('click', () => {
     console.log('clicked');
-    console.log($(this).children('.main-nav__sub-menu').html());
+    console.log($(this).children('ul.main-nav__sub-menu').html());
     // $(this).children($dropdownSubMenu).toggleClass(activeSubMenu);
 
     // $(this).find($dropdownIcon).toggleClass(activeDropdownIcon);
