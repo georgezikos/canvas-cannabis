a4cd1d50aa2fe974df76c5d1bcf84ee0af5467b3
georgezikos <george.zikos@icloud.com>
2021-07-24 03:23:52 -0400

    Huh

--- src/components/allDeviceNav/allDeviceNav.js
+++ src/components/allDeviceNav/allDeviceNav.js
@@ -66,14 +66,12 @@ const subMenuHandler = () => {
     }
     $(this).next().toggleClass(activeSubMenu);
     // $(this).find($dropdownIcon).toggleClass(activeDropdownIcon);
-    $(this)
-      .find($dropdownIcon)
-      .velocity(
-        {
-          transform: ['rotateZ(-180deg)', 'rotateZ(0deg)'],
-        },
-        { duration: 1000 }
-      );
+    $(this).find($dropdownIcon).velocity(
+      {
+        transform: 'rotateZ(-180deg)',
+      },
+      { duration: 250 }
+    );
   });
 };
 
