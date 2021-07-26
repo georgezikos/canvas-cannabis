f1419c75dab4d53186a067b636c0265abae6ae31
georgezikos <george.zikos@icloud.com>
2021-07-26 19:19:00 -0400

    WOOPS

--- src/components/allDeviceNav/allDeviceNav.js
+++ src/components/allDeviceNav/allDeviceNav.js
@@ -214,7 +214,7 @@ const menuCloseHandler = () => {
 //   $linksList.height(`${$window.innerHeight()}px`);
 // };
 const setLinksHeight = () => {
-  $linksList.height(`${iosInnerHeight()}px`);
+  $linksList.height(`${iosInnerHeight() - 104}px`);
 };
 $window.on('resize', setLinksHeight);
 
