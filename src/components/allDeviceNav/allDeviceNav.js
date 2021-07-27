fade0bcef734260426281e69edfd22425fc4851d
georgezikos <george.zikos@icloud.com>
2021-07-27 09:28:34 -0400

    Oh no

--- src/components/allDeviceNav/allDeviceNav.js
+++ src/components/allDeviceNav/allDeviceNav.js
@@ -201,8 +201,8 @@ const menuCloseHandler = () => {
         return;
       }
       if (
-        currentScroll > lastScroll ||
-        (currentScroll > 0 && $dropdownSubMenu.hasClass(activeSubMenu))
+        currentScroll > lastScroll &&
+        $dropdownSubMenu.hasClass(activeSubMenu)
       ) {
         // down
         //prettier-ignore
