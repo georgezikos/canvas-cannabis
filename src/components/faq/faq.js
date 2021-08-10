3e058ba7537a00e5243303d9f9079edb0ed99d3f
georgezikos <george.zikos@icloud.com>
2021-08-10 16:31:32 -0400

    Re-applying animations to FAQ toggle icons

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -1,3 +1,4 @@
+import gsap from 'gsap/gsap-core';
 import './faq.css';
 
 // Selectors
@@ -18,8 +19,10 @@ const faq = () => {
     // prettier-ignore
     if ($this.find($toggleIcon).hasClass(activeToggleIcon)) {
       $this.find($toggleIcon).removeClass(activeToggleIcon);
+      gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 90 }, { duration: 0.25, rotation: 0 });
     } else {
       $this.find($toggleIcon).addClass(activeToggleIcon);
+      gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 0 }, { duration: 0.25, rotation: 90 });
     }
   });
 };
