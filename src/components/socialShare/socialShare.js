c6db710f0d7d660488bf50bc7bc91bb21702f7aa
georgezikos <george.zikos@icloud.com>
2021-08-12 12:16:52 -0400

    Refactoring and cleaning up code for social share links

--- src/components/socialShare/socialShare.js
+++ src/components/socialShare/socialShare.js
@@ -1,42 +1,39 @@
+// Global
+const $body = $('body');
+
+// URL
+let currentPost = $(location).attr('href'); // for copying
+let currentPostEncoded = encodeURIComponent($(location).attr('href')); // for share links
+
+// Share buttons
 const $facebookShareBtn = $('.blog-post__share-link--facebook');
 const $linkedInShareBtn = $('.blog-post__share-link--linkedin');
 const $emailShareBtn = $('.blog-post__share-link--email');
-const facebookShareLink = 'https://www.facebook.com/sharer/sharer.php?u=';
-let currentPost = encodeURIComponent($(location).attr('href'));
+const $copyShareBtn = $('.blog-post__share-link--copy');
 
-// construct the linkedin URL in this order, checkout their docs to understand what each parameter is doing
-const linkedInShareLinkBase =
-  'https://www.linkedin.com/shareArticle?mini=true&url=';
-const linkedInShareLinkTitle = '&title=Canvas%20Cannabis';
-const linkedInShareLinkSource = '&source=https%3A%2F%2Fcanvascannabis.ca%2F';
-const linkedInShareLinkSummary = '&summary=Short%20summary';
+// Share links
+const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${currentPostEncoded}`;
+const linkedInShareLink = `https://www.linkedin.com/shareArticle?mini=true&url=${currentPostEncoded}&title=Canvas%20Cannabis&source=https%3A%2F%2Fcanvascannabis.ca%2F&summary=Short%20summary`; // make title, source and summary parameters dynamic
+const emailShareLink = `mailto:?subject=test&body=${currentPost}`; // gmail on ios is tricky with mailto in general + clean up body variable and subject
 
-const emailShareLink = 'mailto:?subject=test&body='; // ios gmail is a bitch
+// Copy functionality
+const $temp = $('<input>');
 
-let $temp = $('<input>');
-
-// const linkedInShareLink = {
-//   base: ,
-//   url: currentPost,
-//   title: ,
-//   source: ,
-//   summary: ,
-// }
+// Functions
+const copyLink = () => {
+  $copyShareBtn.on('click', () => {
+    $body.append($temp);
+    $temp.val(currentPost).select();
+    document.execCommand('copy');
+    // confirm copied through a tooltip?
+  });
+};
 
 const socialShare = () => {
-  $facebookShareBtn.attr('href', `${facebookShareLink}${currentPost}`);
-  $linkedInShareBtn.attr(
-    'href',
-    `${linkedInShareLinkBase}${currentPost}${linkedInShareLinkTitle}${linkedInShareLinkSource}${linkedInShareLinkSummary}`
-  );
-  $emailShareBtn.attr('href', `${emailShareLink}${currentPost}`);
-  $('.blog-post__share-link--copy').on('click', function () {
-    $('body').append($temp);
-    $temp.val($(location).attr('href')).select();
-    document.execCommand('copy');
-    $temp.remove();
-    // $("p").text("URL copied!");
-  });
+  $facebookShareBtn.attr('href', facebookShareLink);
+  $linkedInShareBtn.attr('href', linkedInShareLink);
+  $emailShareBtn.attr('href', emailShareLink);
+  copyLink();
 };
 
 export default socialShare;
