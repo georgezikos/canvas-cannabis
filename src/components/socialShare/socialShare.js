54e4b2f09c1084ebbeca35539b3895a5ff8f9ba1
georgezikos <george.zikos@icloud.com>
2021-08-12 11:27:08 -0400

    Trying the old syntax

--- src/components/socialShare/socialShare.js
+++ src/components/socialShare/socialShare.js
@@ -1,16 +1,16 @@
 const $facebookShareBtn = $('.blog-post__share-link--facebook');
 const $linkedInShareBtn = $('.blog-post__share-link--linkedin');
 const facebookShareLink = 'https://www.facebook.com/sharer/sharer.php?u=';
-const linkedInShareLink =
-  'https://www.linkedin.com/sharing/share-offsite/?url=';
+// const linkedInShareLink =
+//   'https://www.linkedin.com/sharing/share-offsite/?url=';
 let currentPost = encodeURIComponent($(location).attr('href'));
 
 // construct the linkedin URL in this order, checkout their docs to understand what each parameter is doing
-// const linkedInShareLinkBase =
-//   'https://www.linkedin.com/shareArticle?mini=true&url=';
-// const linkedInShareLinkTitle = '&title=Canvas%20Cannabis';
-// const linkedInShareLinkSource = '&source=https%3A%2F%2Fcanvascannabis.ca%2F';
-// const linkedInShareLinkSummary = '&summary=Short%20summary';
+const linkedInShareLinkBase =
+  'https://www.linkedin.com/shareArticle?mini=true&url=';
+const linkedInShareLinkTitle = '&title=Canvas%20Cannabis';
+const linkedInShareLinkSource = '&source=https%3A%2F%2Fcanvascannabis.ca%2F';
+const linkedInShareLinkSummary = '&summary=Short%20summary';
 
 // const linkedInShareLink = {
 //   base: ,
@@ -22,7 +22,10 @@ let currentPost = encodeURIComponent($(location).attr('href'));
 
 const socialShare = () => {
   $facebookShareBtn.attr('href', `${facebookShareLink}${currentPost}`);
-  $linkedInShareBtn.attr('href', `${linkedInShareLink}${currentPost}`);
+  $linkedInShareBtn.attr(
+    'href',
+    `${linkedInShareLinkBase}${currentPost}${linkedInShareLinkTitle}${linkedInShareLinkSource}${linkedInShareLinkSummary}`
+  );
 };
 
 export default socialShare;
