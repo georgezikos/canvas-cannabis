fdda74116b6357b8bef7d07d427ec50803f31bcb
georgezikos <george.zikos@icloud.com>
2021-08-11 23:22:44 -0400

    Giving LinkedIn a try

--- src/components/socialShare/socialShare.js
+++ src/components/socialShare/socialShare.js
@@ -1,15 +1,20 @@
 const $facebookShareBtn = $('.blog-post__share-link--facebook');
 const $linkedInShareBtn = $('.blog-post__share-link--linkedin');
 const facebookShareLink = 'https://www.facebook.com/sharer/sharer.php?u=';
+// construct the linkedin URL in this order
 const linkedInShareLinkBase =
   'https://www.linkedin.com/shareArticle?mini=true&url=';
-const linkedInShareLinkTitle = 'Canvas%20Cannabis';
+const linkedInShareLinkTitle = '&title=Canvas%20Cannabis';
 const linkedInShareLinkSource = '&source=https%3A%2F%2Fcanvascannabis.ca%2F';
 const linkedInShareLinkSummary = '&summary=Short%20summary';
 let currentPost = encodeURIComponent($(location).attr('href'));
 
 const socialShare = () => {
   $facebookShareBtn.attr('href', `${facebookShareLink}${currentPost}`);
+  $linkedInShareBtn.attr(
+    'href',
+    `${linkedInShareLinkBase}${currentPost}${linkedInShareLinkTitle}${linkedInShareLinkSource}${linkedInShareLinkSummary}`
+  );
 };
 
 export default socialShare;
