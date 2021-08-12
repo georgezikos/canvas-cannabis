093719af4971faaf54f694c1465ce76f4e7e4e47
georgezikos <george.zikos@icloud.com>
2021-08-11 22:59:53 -0400

    Testing Facebook share link

--- src/components/socialShare/socialShare.js
+++ src/components/socialShare/socialShare.js
@@ -1,5 +1,15 @@
+const $facebookShareBtn = $('.blog-post__share-link--facebook');
+const $linkedInShareBtn = $('.blog-post__share-link--linkedin');
+const facebookShareLink = 'https://www.facebook.com/sharer/sharer.php?u=';
+const linkedInShareLinkBase =
+  'https://www.linkedin.com/shareArticle?mini=true&url=';
+const linkedInShareLinkTitle = 'Canvas%20Cannabis';
+const linkedInShareLinkSource = '&source=https%3A%2F%2Fcanvascannabis.ca%2F';
+const linkedInShareLinkSummary = '&summary=Short%20summary';
+let currentPost = encodeURIComponent($(location).attr('href'));
+
 const socialShare = () => {
-  console.log(encodeURIComponent($(location).attr('href')));
+  $facebookShareBtn.attr('href', `${facebookShareLink}${currentPost}`);
 };
 
 export default socialShare;
