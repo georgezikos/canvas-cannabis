b3337b9a72a164e3c5b7a5393c08712dedf14620
georgezikos <george.zikos@icloud.com>
2021-08-12 22:15:28 -0400

    Real stickler

--- src/components/socialShare/socialShare.js
+++ src/components/socialShare/socialShare.js
@@ -18,15 +18,7 @@ const $currentPostSummaryEncoded = encodeURIComponent($currentPostSummary);
 
 // Emails
 const emailSubject = `Canvas Cannabis Blog: ${$currentPostTitle}`;
-const emailBody = `
-  Hey, 
-  %0D%0A
-  %0D%0A
-  I wanted to share this article with you on ${$currentPostTitle} from Canvas Cannabis.
-  %0D%0A
-  %0D%0A
-  You can check it out right here: <a href="${$currentPost}">${$currentPost}</a>
-`;
+const emailBody = `Hey,%0D%0A%0D%0A I wanted to share this article with you on ${$currentPostTitle} from Canvas Cannabis.%0D%0A%0D%0AYou can check it out right here: ${$currentPost}%0D%0A%0D%0AEnjoy!`;
 
 // Share buttons
 const $facebookShareBtn = $('.blog-post__share-link--facebook');
