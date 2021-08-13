f7e95b3bba45a6b9be6903709c36bcfa633ca32f
georgezikos <george.zikos@icloud.com>
2021-08-13 17:04:52 -0400

    Finished logic for Daily Specials section, can be re-factored at some point

--- src/components/dailySpecials/dailySpecials.js
+++ src/components/dailySpecials/dailySpecials.js
@@ -3,7 +3,7 @@ const $dailySpecialsHeading = $('h2.daily-specials__heading');
 const $dailySpecialsBody = $('p.daily-specials__body');
 const $dailySpecialsBtn = $('#daily-specials-btn');
 
-// Days
+// Specials related
 const specials = {
   weekdays: {
     heading: 'Daily specials',
@@ -41,39 +41,44 @@ const specials = {
   },
 };
 
-// Date
+// Date related
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
 
-// Other
-
 const dailySpecials = () => {
   if (currentDay === 0 || currentDay === 6 || currentDay === 5) {
+    // refactor into a function that takes all of these repeating variables as arguments
+    // see if I can use ternarys in place of some of these variables
     $dailySpecialsHeading.text(specials.weekends.heading);
     $dailySpecialsBody.text(
-      `${
-        specials.weekends.prefix
-      } ${specials.weekends.days.weekends.label.toLowerCase()} we feature our favourite ${
-        specials.weekends.days.weekends.products
-      } at 15% off their normal price. Shop today's deals below or visit your closest Canvas Cannabis location.`
+      `${specials.weekends.prefix} ${specials.weekends.days.weekends.label} we feature our favourite ${specials.weekends.days.weekends.products} at 15% off their normal price. Shop today's deals below or visit your closest Canvas Cannabis location.`
     );
     $dailySpecialsBtn.text(`Shop ${specials.weekends.cta}`);
+  } else if (currentDay === 4) {
+    $dailySpecialsHeading.text(specials.weekdays.heading);
+    $dailySpecialsBody.text(
+      `${specials.weekdays.prefix} ${specials.weekdays.days.thursdays.label} we feature our favourite ${specials.weekdays.days.thursdays.products} at 15% off their normal price. Shop today's deals below or visit your closest Canvas Cannabis location.`
+    );
+    $dailySpecialsBtn.text(`Shop ${specials.weekdays.cta}`);
+  } else if (currentDay === 3) {
+    $dailySpecialsHeading.text(specials.weekdays.heading);
+    $dailySpecialsBody.text(
+      `${specials.weekdays.prefix} ${specials.weekdays.days.wednesdays.label} we feature our favourite ${specials.weekdays.days.wednesdays.products} at 15% off their normal price. Shop today's deals below or visit your closest Canvas Cannabis location.`
+    );
+    $dailySpecialsBtn.text(`Shop ${specials.weekdays.cta}`);
+  } else if (currentDay === 2) {
+    $dailySpecialsHeading.text(specials.weekdays.heading);
+    $dailySpecialsBody.text(
+      `${specials.weekdays.prefix} ${specials.weekdays.days.tuesdays.label} we feature our favourite ${specials.weekdays.days.tuesdays.products} at 15% off their normal price. Shop today's deals below or visit your closest Canvas Cannabis location.`
+    );
+    $dailySpecialsBtn.text(`Shop ${specials.weekdays.cta}`);
+  } else if (currentDay === 1) {
+    $dailySpecialsHeading.text(specials.weekdays.heading);
+    $dailySpecialsBody.text(
+      `${specials.weekdays.prefix} ${specials.weekdays.days.mondays.label} we feature our favourite ${specials.weekdays.days.mondays.products} at 15% off their normal price. Shop today's deals below or visit your closest Canvas Cannabis location.`
+    );
+    $dailySpecialsBtn.text(`Shop ${specials.weekdays.cta}`);
   }
-  // if (currentDay === 0 || currentDay === 6) {
-  //   console.log('Not today, freak!');
-  // } else if (currentDay === 1 || currentDay === 5) {
-  //   $mondaySpecials.css('display', 'flex');
-  //   console.log('Monday!');
-  // } else if (currentDay === 2) {
-  //   $tuesdaySpecials.css('display', 'flex');
-  //   console.log('Tuesday!');
-  // } else if (currentDay === 3) {
-  //   $wednesdaySpecials.css('display', 'flex');
-  //   console.log('Wednesday!');
-  // } else if (currentDay === 4) {
-  //   $thursdaySpecials.css('display', 'flex');
-  //   console.log('Thursday!');
-  // }
 };
 
 export default dailySpecials;
