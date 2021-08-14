7e6ae4690c91b6b4e48d74fdf30fc007300e28c9
georgezikos <george.zikos@icloud.com>
2021-08-14 16:53:13 -0400

    Implementing debounce method from Lodash

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -11,21 +11,24 @@ const activeToggleButton = 'faq__question-container--active';
 const activeAnswer = 'faq__answer-container--active';
 const activeToggleIcon = 'faq__toggle-icon--active';
 
+// Functions
+const faqHandler = () => {
+  console.log('clicked');
+  const $this = $(this);
+  $this.toggleClass(activeToggleButton);
+  $this.next().toggleClass(activeAnswer);
+  // prettier-ignore
+  if ($this.find($toggleIcon).hasClass(activeToggleIcon)) {
+    $this.find($toggleIcon).removeClass(activeToggleIcon);
+    gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 90 }, { duration: 0.25, rotation: 0 });
+  } else {
+    $this.find($toggleIcon).addClass(activeToggleIcon);
+    gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 0 }, { duration: 0.25, rotation: 90 });
+  }
+};
+
 const faq = () => {
-  $toggleButton.on('click', function () {
-    console.log('clicked');
-    const $this = $(this);
-    $this.toggleClass(activeToggleButton);
-    $this.next().toggleClass(activeAnswer);
-    // prettier-ignore
-    if ($this.find($toggleIcon).hasClass(activeToggleIcon)) {
-      $this.find($toggleIcon).removeClass(activeToggleIcon);
-      gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 90 }, { duration: 0.25, rotation: 0 });
-    } else {
-      $this.find($toggleIcon).addClass(activeToggleIcon);
-      gsap.fromTo($this.find($toggleIcon), { duration: 0.25, rotation: 0 }, { duration: 0.25, rotation: 90 });
-    }
-  });
+  $toggleButton.on('click', _debounce(faqHandler, 200));
 };
 
 export default faq;
