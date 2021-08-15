cc3d7ceb1636be6b913f464141e884e867e887ef
georgezikos <george.zikos@icloud.com>
2021-08-15 11:42:42 -0400

    Experimenting with showing the segmentation modal

--- src/components/modals/shopSegmentation/shopSegmentation.js
+++ src/components/modals/shopSegmentation/shopSegmentation.js
@@ -7,17 +7,75 @@ const $segmentationModal = $('.modal--shop'); // containing element
 const $segmentationForm = $('.modal__form--shop');
 const $chooseStore = $('#shop-segmentation-select');
 
+// Buttons
+const $navShop = '.main-nav__link--shop'; // nav 'shop' link
+const $shopNow = '#shop-now-btn'; // hero 'shop now'
+const $dailySpecialsShop = '#daily-specials-btn'; // daily specials/staff picks
+const $shopFlower = '#shop-flower-btn'; // shop flower
+const $shopEdibles = '#shop-edibles-btn'; // shop edibles
+const $shopAccessories = '#shop-accessories-btn'; // shop accessories
+
 // Classes
 const activeSegmentation = 'modal--active';
 const disableScroll = 'active-modal';
 
+// URLs
+const dutchieMenu = 'https://dutchie.com/embedded-menu'; // top-level
+// Stores
+const danforth = 'canvas'; // danforth main
+const liberty = 'canvas-liberty'; // liberty main
+const mountDennis = 'canvas-weston'; // mount dennis main
+
+// Paths
+const specials = 'specials'; // sale category
+const flower = 'flower'; // flower category
+const edibles = 'edibles'; // flower category
+const accessories = 'accessories'; // flower category
+
+// const firstHalf;
+
 // Functions
+// const pathConstructor = (store, path, products = false, main = false) => { // how to make some of these args optional? Only really need to know the store and if main is true
+//   const destination;
+//   if (main) { // convert to switch-case?
+//     destination = `${dutchieMenu}/${store}`;
+//   } else if (!products) {
+//     destination = `${dutchieMenu}/${store}/products/${path}`;
+//   } else if (products) {
+//     destination = `${dutchieMenu}/${store}/${path}`;
+//   }
+//   return destination;
+// }
 
-// engaging modal, dealing with underlying elements
-$segmentationModal.addClass(activeSegmentation);
-$html.addClass(disableScroll);
-$body.addClass(disableScroll);
+// const segmentationHandler = (store, path, products, main) => {
+//   const shopPath = pathConstructor(store, path, products, main);
+//   // add classes to modal, html and body elements
+//   // call the pathConstructor function and hold the value in a new variable
+//   // on submit prevent default
+//   // navigate to that URL in a new tab
+//   // clear the form? // if yes, should this logic be included in the age gate?
+//   // remove the active classes from the modal, html and body elements
+// }
 
-const shopSegmentation = () => {};
+const test = () => {
+  $shopNow.on('click', () => {
+    // launch the modal
+    $segmentationModal.addClass(activeSegmentation);
+    $html.addClass(disableScroll);
+    $body.addClass(disableScroll);
+    // construct part of the url
+    // if shop now or shop link main is true
+    // if the others capture the path
+  });
+};
+
+$segmentationForm.on('submit', () => {
+  // construct the path
+  // redirect to the URL
+});
+
+const shopSegmentation = () => {
+  test();
+};
 
 export default shopSegmentation;
