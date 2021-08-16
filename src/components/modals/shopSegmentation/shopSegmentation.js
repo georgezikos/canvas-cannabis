f4c03e0af0e82ca9ee82e80e3afcd1dfb4d33d51
georgezikos <george.zikos@icloud.com>
2021-08-16 12:08:05 -0400

    Some quick refactoring, just want to make sure nothing is broken

--- src/components/modals/shopSegmentation/shopSegmentation.js
+++ src/components/modals/shopSegmentation/shopSegmentation.js
@@ -1,15 +1,18 @@
 import '../modals.css';
 
 // Selectors
+// Global
 const $body = $('body');
 const $html = $('html');
+
+// Modal
 const $segmentationModal = $('.modal--shop'); // containing element
 const $segmentationForm = $('.modal__form--shop');
-const $chooseStore = $('#shop-segmentation-select');
+const $storeSelect = $('#shop-segmentation-select');
 
 // Buttons
 const $navShop = $('.main-nav__link--shop'); // nav 'shop' link
-const $shopNow = $('#shop-now-btn'); // hero 'shop now'
+const $heroShop = $('#shop-now-btn'); // hero 'shop now'
 const $dailySpecialsShop = $('#daily-specials-btn'); // daily specials/staff picks
 const $shopFlower = $('#shop-flower-btn'); // shop flower
 const $shopEdibles = $('#shop-edibles-btn'); // shop edibles
@@ -19,121 +22,109 @@ const $shopAccessories = $('#shop-accessories-btn'); // shop accessories
 const activeSegmentation = 'modal--active';
 const disableScroll = 'active-modal';
 
-// URLs
+// URL Parts
 const dutchieTopLevel = 'https://dutchie.com/embedded-menu'; // top-level
+
 // Stores
 const danforth = 'canvas'; // danforth main
 const liberty = 'canvas-liberty'; // liberty main
 const mountDennis = 'canvas-weston'; // mount dennis main
 
-// Paths
-const specials = 'specials'; // sale category
+// Destinations
+const specials = 'specials'; // specials category
 const flower = 'flower'; // flower category
 const edibles = 'edibles'; // flower category
 const accessories = 'accessories'; // flower category
 
-// Add 'Shop Now' mobile nav button to this mix
-// Add click away from modal to close functionality
-// Add esc to close functionality
-// Disable the placeholder value and change it's color to 30% black
-// How to launch without repainting/scrolling to the top of the page
-// Collapse open menu if the modal is triggered
-
-let destination;
-let path;
+// URL Variables
+let fullPath;
+let main = false;
 let store;
 let products = false;
-let main = false;
+let destination;
 
 // Functions
-const pathConstructor = (store, path, products, main) => {
-  // how to make some of these args optional? Only really need to know the store and if main is true
+const pathConstructor = (main, store, products, destination) => {
   if (main) {
     // convert to switch-case?
-    destination += `/${store}`;
-  } else if (!products) {
-    destination = `${dutchieTopLevel}/${store}/products/${path}`;
+    fullPath += `/${store}`;
   } else if (products) {
-    destination = `${dutchieTopLevel}/${store}/${path}`;
+    fullPath = `${dutchieTopLevel}/${store}/${destination}`;
+  } else if (!products) {
+    fullPath = `${dutchieTopLevel}/${store}/products/${destination}`; // put products in a constant?
   }
-  console.log(destination);
+  console.log(fullPath);
 };
 
-// const segmentationHandler = (store, path, products, main) => {
-//   const shopPath = pathConstructor(store, path, products, main);
-//   // add classes to modal, html and body elements
-//   // call the pathConstructor function and hold the value in a new variable
-//   // on submit prevent default
-//   // navigate to that URL in a new tab
-//   // clear the form? // if yes, should this logic be included in the age gate?
-//   // remove the active classes from the modal, html and body elements
-// }
-
 const shopSegmentation = () => {
-  $shopNow.on('click', function () {
-    // launch the modal
+  $heroShop.on('click', function () {
+    // launch the modal – convert into it's own function
     $segmentationModal.addClass(activeSegmentation);
     $html.addClass(disableScroll);
     $body.addClass(disableScroll);
-    // capture value of data attr
+    // capture value of data attr – try the alternative to 'this' from the docs
     const dataVal = $(this).data('menu');
-    // construct part of the url
+    // construct part of the url – turn this into it's own function and switch-case?
     if (dataVal === 'main') {
-      destination = dutchieTopLevel;
+      fullPath = dutchieTopLevel;
       main = true;
-    } else if (dataVal === 'sale') {
-      path = specials;
+    } else if (dataVal === 'specials') {
+      destination = specials;
     } else if (dataVal === 'flower') {
-      path = flower;
+      products = true;
+      destination = flower;
     } else if (dataVal === 'edibles') {
-      path = edibles;
+      products = true;
+      destination = edibles;
     } else if (dataVal === 'accessories') {
-      path = accessories;
+      products = true;
+      destination = accessories;
     }
-    !destination ? console.log(path) : console.log(destination);
+    !fullPath ? console.log(destination) : console.log(fullPath); // remove
   });
   $navShop.on('click', function () {
-    // launch the modal
+    // launch the modal – convert into it's own function
     $segmentationModal.addClass(activeSegmentation);
     $html.addClass(disableScroll);
     $body.addClass(disableScroll);
-    // capture value of data attr
+    // capture value of data attr – try the alternative to 'this' from the docs
     const dataVal = $(this).data('menu');
-    // construct part of the url
+    // construct part of the url – turn this into it's own function and switch-case?
     if (dataVal === 'main') {
-      destination = dutchieTopLevel;
+      fullPath = dutchieTopLevel;
       main = true;
-    } else if (dataVal === 'sale') {
-      path = specials;
+    } else if (dataVal === 'specials') {
+      destination = specials;
     } else if (dataVal === 'flower') {
-      path = flower;
+      destination = flower;
     } else if (dataVal === 'edibles') {
-      path = edibles;
+      destination = edibles;
     } else if (dataVal === 'accessories') {
-      path = accessories;
+      destination = accessories;
     }
-    !destination ? console.log(path) : console.log(destination);
+    !fullPath ? console.log(destination) : console.log(fullPath); // remove
   });
   $dailySpecialsShop.on('click', function () {
-    // launch the modal
+    // launch the modal – convert into it's own function
     $segmentationModal.addClass(activeSegmentation);
     $html.addClass(disableScroll);
     $body.addClass(disableScroll);
-    // capture value of data attr
+    // capture value of data attr – try the alternative to 'this' from the docs
     const dataVal = $(this).data('menu');
-    // construct part of the url
+    // construct part of the url – turn this into it's own function and switch-case?
     if (dataVal === 'main') {
-      destination = dutchieTopLevel;
-    } else if (dataVal === 'sale') {
-      path = specials;
+      fullPath = dutchieTopLevel;
+      main = true;
+    } else if (dataVal === 'specials') {
+      destination = specials;
     } else if (dataVal === 'flower') {
-      path = flower;
+      destination = flower;
     } else if (dataVal === 'edibles') {
-      path = edibles;
+      destination = edibles;
     } else if (dataVal === 'accessories') {
-      path = accessories;
+      destination = accessories;
     }
-    !destination ? console.log(path) : console.log(destination);
+    !fullPath ? console.log(destination) : console.log(fullPath); // remove
   });
   $shopFlower.on('click', function () {
     // launch the modal
@@ -144,20 +135,20 @@ const shopSegmentation = () => {
     const dataVal = $(this).data('menu');
     // construct part of the url
     if (dataVal === 'main') {
-      destination = dutchieTopLevel;
-    } else if (dataVal === 'sale') {
-      path = specials;
+      fullPath = dutchieTopLevel;
+    } else if (dataVal === 'specials') {
+      destination = specials;
     } else if (dataVal === 'flower') {
-      path = flower;
+      destination = flower;
       products = true;
     } else if (dataVal === 'edibles') {
-      path = edibles;
+      destination = edibles;
       products = true;
     } else if (dataVal === 'accessories') {
-      path = accessories;
+      destination = accessories;
       products = true;
     }
-    !destination ? console.log(path) : console.log(destination);
+    !fullPath ? console.log(destination) : console.log(fullPath);
   });
   $shopEdibles.on('click', function () {
     // launch the modal
@@ -168,20 +159,20 @@ const shopSegmentation = () => {
     const dataVal = $(this).data('menu');
     // construct part of the url
     if (dataVal === 'main') {
-      destination = dutchieTopLevel;
-    } else if (dataVal === 'sale') {
-      path = specials;
+      fullPath = dutchieTopLevel;
+    } else if (dataVal === 'specials') {
+      destination = specials;
     } else if (dataVal === 'flower') {
-      path = flower;
+      destination = flower;
       products = true;
     } else if (dataVal === 'edibles') {
-      path = edibles;
+      destination = edibles;
       products = true;
     } else if (dataVal === 'accessories') {
-      path = accessories;
+      destination = accessories;
       products = true;
     }
-    !destination ? console.log(path) : console.log(destination);
+    !fullPath ? console.log(destination) : console.log(fullPath);
   });
   $shopAccessories.on('click', function () {
     // launch the modal
@@ -192,31 +183,39 @@ const shopSegmentation = () => {
     const dataVal = $(this).data('menu');
     // construct part of the url
     if (dataVal === 'main') {
-      destination = dutchieTopLevel;
-    } else if (dataVal === 'sale') {
-      path = specials;
+      fullPath = dutchieTopLevel;
+    } else if (dataVal === 'specials') {
+      destination = specials;
     } else if (dataVal === 'flower') {
-      path = flower;
+      destination = flower;
       products = true;
     } else if (dataVal === 'edibles') {
-      path = edibles;
+      destination = edibles;
       products = true;
     } else if (dataVal === 'accessories') {
-      path = accessories;
+      destination = accessories;
       products = true;
     }
-    !destination ? console.log(path) : console.log(destination);
+    !fullPath ? console.log(destination) : console.log(fullPath);
   });
   $segmentationForm.on('submit', function (e) {
     e.preventDefault();
     // construct the path
-    const storeSelect = $chooseStore.val();
+    const storeSelect = $storeSelect.val();
     if (storeSelect === 'danforth') {
+      // put 'danforth' in a constant?
       store = danforth;
-      pathConstructor(store, path, products, main);
+      pathConstructor(store, destination, products, main);
     }
     // redirect to the URL
   });
 };
 
 export default shopSegmentation;
+
+// Add 'Shop Now' mobile nav button to this mix
+// Add click away from modal to close functionality
+// Add esc to close functionality
+// Disable the placeholder value and change it's color to 30% black
+// How to launch without repainting/scrolling to the top of the page
+// Collapse open menu if the modal is triggered
