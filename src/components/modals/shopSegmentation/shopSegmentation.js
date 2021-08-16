9f0d0e38837916e89a4b55eac96d12f6b72d2c33
georgezikos <george.zikos@icloud.com>
2021-08-16 14:26:55 -0400

    Turns out I do still need to clear the value from the form

--- src/components/modals/shopSegmentation/shopSegmentation.js
+++ src/components/modals/shopSegmentation/shopSegmentation.js
@@ -213,7 +213,7 @@ const shopSegmentation = () => {
       store = danforth;
       const fullPath = pathConstructor(main, store, products, destination);
       window.open(fullPath);
-      // $storeSelect.prop('selectedIndex', 0); // resets the select field
+      $storeSelect.prop('selectedIndex', 0); // resets the select field
       // destroy the modal – convert into it's own function
       $segmentationModal.removeClass(activeSegmentation);
       $html.removeClass(disableScroll);
@@ -222,7 +222,7 @@ const shopSegmentation = () => {
       store = liberty;
       const fullPath = pathConstructor(main, store, products, destination);
       window.open(fullPath);
-      // $storeSelect.prop('selectedIndex', 0); // resets the select field
+      $storeSelect.prop('selectedIndex', 0); // resets the select field
       // destroy the modal – convert into it's own function
       $segmentationModal.removeClass(activeSegmentation);
       $html.removeClass(disableScroll);
@@ -231,7 +231,7 @@ const shopSegmentation = () => {
       store = mountDennis;
       const fullPath = pathConstructor(main, store, products, destination);
       window.open(fullPath);
-      // $storeSelect.prop('selectedIndex', 0); // resets the select field
+      $storeSelect.prop('selectedIndex', 0); // resets the select field
       // destroy the modal – convert into it's own function
       $segmentationModal.removeClass(activeSegmentation);
       $html.removeClass(disableScroll);
