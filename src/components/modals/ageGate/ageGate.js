2052b2dc786dc0649e4eb742816e4e255b179b25
georgezikos <george.zikos@icloud.com>
2021-08-17 11:17:40 -0400

    Is this even woooorking

--- src/components/modals/ageGate/ageGate.js
+++ src/components/modals/ageGate/ageGate.js
@@ -39,6 +39,7 @@ const validateAge = () => {
     // If they haven't verified their age, they are unable to clear the age gate
     // input type checkbox is invalid, target previous sibling .form__checkbox with styles .css('border-color', '#F3806D');
     console.dir($verifyAge.prev());
+    console.log('Helllo');
     return;
   } else if ($verifyAge.prop('checked') && !$rememberMe.prop('checked')) {
     // If they only verify their age, but do not wanted to be remembered, let them in and create a cookie to account for repeat visits within a 24-hour period and clear the age gate
