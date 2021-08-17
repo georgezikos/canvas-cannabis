dc40356b31168b723534b1c15d7a37e4e0497c5d
georgezikos <george.zikos@icloud.com>
2021-08-17 11:30:21 -0400

    Seriously confusing

--- src/components/modals/ageGate/ageGate.js
+++ src/components/modals/ageGate/ageGate.js
@@ -37,7 +37,6 @@ const cookiesOrLegal = () => {
 const validateAge = () => {
   if (!$verifyAge.prop('checked')) {
     // If they haven't verified their age, they are unable to clear the age gate
-    console.log('WTF BRO');
     return;
   } else if ($verifyAge.prop('checked') && !$rememberMe.prop('checked')) {
     // If they only verify their age, but do not wanted to be remembered, let them in and create a cookie to account for repeat visits within a 24-hour period and clear the age gate
@@ -53,6 +52,8 @@ const validateAge = () => {
     $html.removeClass(disableScroll);
     $body.removeClass(disableScroll);
     return;
+  } else if (!$verifyAge.prop('checked') && !$rememberMe.prop('checked')) {
+    console.log('WTF BRO');
   }
 };
 
