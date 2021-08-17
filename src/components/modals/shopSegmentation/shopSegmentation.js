a255ae3da89aa9f5cce161085ab982e305309b7a
georgezikos <george.zikos@icloud.com>
2021-08-16 20:29:00 -0400

    Going to leave that functionality for tomorrow, need a fresh perspective

--- src/components/modals/shopSegmentation/shopSegmentation.js
+++ src/components/modals/shopSegmentation/shopSegmentation.js
@@ -57,40 +57,40 @@ const pathConstructor = (main, store, products, destination) => {
 };
 
 // See if I can make these functions more dynamic and import them between nav component and here
-const closeModalHandler = () => {
-  // ESC key to close menu
-  const escClose = () => {
-    $document.keyup((e) => {
-      const escKey = 27;
-      if (
-        $segmentationModal.hasClass(activeSegmentation) &&
-        e.keyCode === escKey
-      ) {
-        $segmentationModal.removeClass(activeSegmentation);
-        $html.removeClass(disableScroll);
-        $body.removeClass(disableScroll);
-      }
-    });
-  };
-  // Clicking away from nav to close open sub-menus
-  const clickAwayClose = () => {
-    $document.on('click', (e) => {
-      let $target = $(e.target);
-      if (
-        $segmentationModal.hasClass(activeSegmentation) &&
-        !$target.closest($segmentationModal).length
-      ) {
-        $segmentationModal.removeClass(activeSegmentation);
-        $html.removeClass(disableScroll);
-        $body.removeClass(disableScroll);
-      } else {
-        return;
-      }
-    });
-  };
-  escClose();
-  clickAwayClose();
-};
+// const closeModalHandler = () => {
+//   // ESC key to close menu
+//   const escClose = () => {
+//     $document.keyup((e) => {
+//       const escKey = 27;
+//       if (
+//         $segmentationModal.hasClass(activeSegmentation) &&
+//         e.keyCode === escKey
+//       ) {
+//         $segmentationModal.removeClass(activeSegmentation);
+//         $html.removeClass(disableScroll);
+//         $body.removeClass(disableScroll);
+//       }
+//     });
+//   };
+//   // Clicking away from nav to close open sub-menus
+//   const clickAwayClose = () => {
+//     $document.on('click', (e) => {
+//       let $target = $(e.target);
+//       if (
+//         $segmentationModal.hasClass(activeSegmentation) &&
+// !$target.closest($segmentationModal).length
+//       ) {
+//         $segmentationModal.removeClass(activeSegmentation);
+//         $html.removeClass(disableScroll);
+//         $body.removeClass(disableScroll);
+//       } else {
+//         return;
+//       }
+//     });
+//   };
+//   escClose();
+//   clickAwayClose();
+// };
 
 const shopSegmentation = () => {
   // Create a placeholder value in the select field
@@ -289,7 +289,7 @@ const shopSegmentation = () => {
       $body.removeClass(disableScroll);
     }
   });
-  closeModalHandler();
+  // closeModalHandler();
 };
 
 export default shopSegmentation;
