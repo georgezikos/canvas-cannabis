0ecb36d6584960b43834c4d75a6f8800cb9f35f5
georgezikos <george.zikos@icloud.com>
2021-08-17 11:36:41 -0400

    If true, WOW

--- src/components/modals/ageGate/ageGate.js
+++ src/components/modals/ageGate/ageGate.js
@@ -37,6 +37,7 @@ const cookiesOrLegal = () => {
 const validateAge = () => {
   if (!$verifyAge.prop('checked')) {
     // If they haven't verified their age, they are unable to clear the age gate
+    console.log('OMG');
     return;
   } else if ($verifyAge.prop('checked') && !$rememberMe.prop('checked')) {
     // If they only verify their age, but do not wanted to be remembered, let them in and create a cookie to account for repeat visits within a 24-hour period and clear the age gate
