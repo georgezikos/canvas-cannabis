f361c38b2eee2f6f29fec2123673a0a5058bdcd0
georgezikos <george.zikos@icloud.com>
2021-09-13 12:28:23 -0400

    Starting to work with the footer opt-in submit button first, to figure out how this is done

--- src/components/buttons/buttonsWaiting.js
+++ src/components/buttons/buttonsWaiting.js
@@ -1,11 +1,22 @@
 import './buttonsWaiting.css';
 
-// target all input types of submit
-const $submitBtns = $('input[type="submit"]');
-// on form submit animate inside the button
+// Button Selectors
+const $footerSubmit = $('#newsletter-subscribe'); // #newsletter-subscribe
+// const $ageGateSubmit // #confirm-age
+// const $segmentationSubmit // #go-to-menu
+// const $jobApplicationSubmit // .main-button.main-button--fixed.is--job-application
 
+// Replacement HTML
+const footerSubmitReplacement = `
+  <button class="main-button main-button--fixed" id="newsletter-subscribe">Subscribe</button>
+`;
+// const ageGateSubmitReplacement
+// const segmentationSubmitReplacement
+// const jobApplicationSubmitReplacement
+
+// on form submit animate inside the button
 const buttonsWaiting = () => {
-  $submitBtns.replaceWith(`<button></button>`);
+  $footerSubmit.replaceWith(footerSubmitReplacement);
 };
 
 export default buttonsWaiting;
