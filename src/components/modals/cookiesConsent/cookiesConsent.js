5f79d02cb42015a66a4c97352fabdff4deebd199
georgezikos <george.zikos@icloud.com>
2021-09-15 11:34:56 -0400

    Setting up cookies for cookies consent prompt

--- src/components/modals/cookiesConsent/cookiesConsent.js
+++ src/components/modals/cookiesConsent/cookiesConsent.js
@@ -1,19 +1,29 @@
-// import { gsap } from 'gsap';
+import { gsap } from 'gsap';
+import Cookies from 'js-cookie';
 
+// Selectors
 const $acceptCookiesBtn = $('#accept-cookies');
 const $cookiesConsentModal = $('.modal--cookies-consent');
 
-// Check for existence of cookie
-// After x amount of scroll translateY into view
-// When dismissed create cookie
-// Fire checkmark animation
-// translateY out of view
+// Cookie Names
+const cookiesConsentCookie = 'acceptCookies';
 
 const cookiesConsent = () => {
-  $acceptCookiesBtn.on('click', function () {
-    // Temporary dismissal functionality
-    $cookiesConsentModal.hide();
-  });
+  if (Cookies.get(cookiesConsentCookie)) {
+    // If the cookies existence returns true, we won't show the cookies consent prompt again for
+    return;
+  } else {
+    // If not, show the prompt
+    $cookiesConsentModal.show();
+    // After x amount of scroll translateY into view
+    $acceptCookiesBtn.on('click', function () {
+      // When dismissed create cookie
+      Cookies.set(cookiesConsentCookie, true, { expires: 30 }); // find out what kind of timeframe we can work with for this
+      // Fire checkmark animation
+      // translateY out of view
+      $cookiesConsentModal.hide();
+    });
+  }
 };
 
 export default cookiesConsent;
