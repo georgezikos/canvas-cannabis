9387a0a61e4fbb25c8490a4d7250c6be03c2f9ce
georgezikos <george.zikos@icloud.com>
2021-09-16 14:15:05 -0400

    Trying to figure out ScrollTrigger with GSAP

--- src/components/modals/cookiesConsent/cookiesConsent.js
+++ src/components/modals/cookiesConsent/cookiesConsent.js
@@ -13,6 +13,31 @@ const cookiesConsentCookie = 'acceptCookies';
 // Classes
 const getConsent = 'is--active';
 
+// Functions
+export const cookiesConsentPrompt = () => {
+  // Age gate has been cleared, show the cookies consent at a specific percentage down the page translateY into view
+  $cookiesConsentModal.toggleClass(getConsent);
+
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to($cookiesConsentModal, {
+    yPercent: 0,
+    ease: 'power1.out',
+    scrollTrigger: {
+      trigger: '.thirds-section--featured-products',
+      scrub: 0.3,
+    },
+  });
+
+  $acceptCookiesBtn.on('click', function () {
+    // When dismissed create cookie
+    Cookies.set(cookiesConsentCookie, true, { expires: 30 });
+    // Fire checkmark animation
+    // translateY out of view
+    // display none;
+    $cookiesConsentModal.toggleClass(getConsent);
+  });
+};
+
 const cookiesConsent = () => {
   if (Cookies.get(cookiesConsentCookie)) {
     // If the cookies existence returns true, we won't show the cookies consent prompt again for 30 days
@@ -23,16 +48,7 @@ const cookiesConsent = () => {
       // Age gate hasn't been clear so don't show the cookies consent yet
       return;
     } else if (!$ageGate.hasClass(activeAgeGate)) {
-      // Age gate has been cleared, show the cookies consent at a specific percentage down the page
-      $cookiesConsentModal.toggleClass(getConsent);
-      // After x amount of scroll translateY into view
-      $acceptCookiesBtn.on('click', function () {
-        // When dismissed create cookie
-        Cookies.set(cookiesConsentCookie, true, { expires: 30 });
-        // Fire checkmark animation
-        // translateY out of view
-        $cookiesConsentModal.toggleClass(getConsent);
-      });
+      cookiesConsentPrompt();
     }
   }
 };
