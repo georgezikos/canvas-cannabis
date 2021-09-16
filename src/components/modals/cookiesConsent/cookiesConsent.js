85efd08b9635bbccb7e4d5cdd9c70d885a7ba707
georgezikos <george.zikos@icloud.com>
2021-09-16 13:22:52 -0400

    Switching from inline styles to class toggling to handle the cookies consent prompt

--- src/components/modals/cookiesConsent/cookiesConsent.js
+++ src/components/modals/cookiesConsent/cookiesConsent.js
@@ -1,3 +1,4 @@
+import '../modals.css';
 import { gsap } from 'gsap';
 import Cookies from 'js-cookie';
 
@@ -8,20 +9,23 @@ const $cookiesConsentModal = $('.modal--cookies-consent');
 // Cookie Names
 const cookiesConsentCookie = 'acceptCookies';
 
+// Classes
+const getConsent = 'is--active';
+
 const cookiesConsent = () => {
   if (Cookies.get(cookiesConsentCookie)) {
-    // If the cookies existence returns true, we won't show the cookies consent prompt again for
+    // If the cookies existence returns true, we won't show the cookies consent prompt again for 30 days
     return;
   } else {
     // If not, show the prompt
-    $cookiesConsentModal.css('display', 'flex');
+    $cookiesConsentModal.toggleClass(getConsent);
     // After x amount of scroll translateY into view
     $acceptCookiesBtn.on('click', function () {
       // When dismissed create cookie
-      Cookies.set(cookiesConsentCookie, true, { expires: 30 }); // find out what kind of timeframe we can work with for this
+      Cookies.set(cookiesConsentCookie, true, { expires: 30 });
       // Fire checkmark animation
       // translateY out of view
-      $cookiesConsentModal.hide();
+      $cookiesConsentModal.toggleClass(getConsent);
     });
   }
 };
