61deb8a12ef5bc0c1d8ba72ee2e189d67313d5b3
georgezikos <george.zikos@icloud.com>
2021-09-26 14:53:56 -0400

    Back in the mix

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -3,7 +3,8 @@ const $storeTitleEl = $('h2.store-locations__heading');
 const storeTitles = () => {
   for (let i = 0; i < $storeTitleEl.length; i++) {
     const storeTitleSplit = $storeTitleEl[i].innerText.split(' ');
-    console.log(storeTitleSplit);
+    const lastWord = storeTitleSplit.pop();
+    console.log(lastWord);
     // $storeTitleEl[i].innerHTML.replace('Dispensary', 'Datspensary');
     // $storeTitleEl[i].html().replace('DISPENSARY', 'DATSPENSARY');
     // const storeTitleLength = storeTitleSplit.length;
