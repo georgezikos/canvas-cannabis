b5ef758ec52afaecbd39a1b39787f61a256769e4
georgezikos <george.zikos@icloud.com>
2021-09-26 13:38:47 -0400

    Detour

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -3,7 +3,8 @@ const $storeTitleEl = $('h2.store-locations__heading');
 const storeTitles = () => {
   for (let i = 0; i < $storeTitleEl.length; i++) {
     // const storeTitleSplit = $storeTitleEl[i].innerText.split(' ');
-    $storeTitleEl[i].html().replace('DISPENSARY', 'DATSPENSARY');
+    console.log($storeTitleEl);
+    // $storeTitleEl[i].html().replace('DISPENSARY', 'DATSPENSARY');
     // const storeTitleLength = storeTitleSplit.length;
     // const lastWord = storeTitleSplit[storeTitleLength - 1];
     // const lastWordBlock = `<span class="line-break">${lastWord}</span>`
