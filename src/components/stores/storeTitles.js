9a2c7f78ec411bdd29afc36ca3e21b535b7ea84f
georgezikos <george.zikos@icloud.com>
2021-09-26 13:34:47 -0400

    Overlooked a couple details

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -3,7 +3,7 @@ const $storeTitleEl = $('h2.store-locations__heading');
 const storeTitles = () => {
   for (let i = 0; i < $storeTitleEl.length; i++) {
     // const storeTitleSplit = $storeTitleEl[i].innerText.split(' ');
-    console.log($storeTitleEl.html().replace('DISPENSARY', 'DATSPENSARY'));
+    $storeTitleEl[i].html().replace('DISPENSARY', 'DATSPENSARY');
     // const storeTitleLength = storeTitleSplit.length;
     // const lastWord = storeTitleSplit[storeTitleLength - 1];
     // const lastWordBlock = `<span class="line-break">${lastWord}</span>`
