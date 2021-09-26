73a4bebb043714be3d6829b1306ce909c725a66c
georgezikos <george.zikos@icloud.com>
2021-09-26 16:02:14 -0400

    Reusing the title formatting function for the store titles on the about page

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -1,13 +1,21 @@
 const $storeTitleEl = $('h2.store-locations__heading');
+const $aboutStoreTitleEl = $('h3.about__stores-heading');
 
 const storeTitles = () => {
-  for (let i = 0; i < $storeTitleEl.length; i++) {
-    const storeTitleSplit = $storeTitleEl[i].innerText.split(' ');
-    const lastWord = storeTitleSplit.pop();
-    const firstHalf = storeTitleSplit.join(' ');
-    const newTitle = `<h2 class="store-locations__heading">${firstHalf} <span class="line-break">${lastWord}</span></h2>`;
-    $storeTitleEl[i].outerHTML = newTitle;
-  }
+  const titleFormatter = (el) => {
+    for (let i = 0; i < el.length; i++) {
+      const storeTitleSplit = el[i].innerText.split(' ');
+      const lastWord = storeTitleSplit.pop();
+      const firstHalf = storeTitleSplit.join(' ');
+      const newTitle =
+        el === $storeTitleEl
+          ? `<h2 class="store-locations__heading">${firstHalf} <span class="line-break">${lastWord}</span></h2>`
+          : `<h3 class="about__stores-heading">${firstHalf} <span class="line-break">${lastWord}</span></h3>`;
+      el[i].outerHTML = newTitle;
+    }
+  };
+  titleFormatter($storeTitleEl);
+  titleFormatter($aboutStoreTitleEl);
 };
 
 export default storeTitles;
