c6ed0cd81ee8fd4a7645e1bc545e592a56272f40
georgezikos <george.zikos@icloud.com>
2021-09-26 13:04:07 -0400

    Things have gone awry

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -2,7 +2,7 @@ const $storeTitle = $('h2.store-locations__heading');
 
 const storeTitles = () => {
   for (let i = 0; i < $storeTitle.length; i++) {
-    console.log($storeTitle[i].text().split());
+    console.log($storeTitle[i]);
   }
 };
 
