6f9fc14ee90661aa81c359b6ac8d5d2a99627ad2
georgezikos <george.zikos@icloud.com>
2021-09-26 13:31:36 -0400

    Shorter solution?

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -2,10 +2,11 @@ const $storeTitleEl = $('h2.store-locations__heading');
 
 const storeTitles = () => {
   for (let i = 0; i < $storeTitleEl.length; i++) {
-    const storeTitleSplit = $storeTitleEl[i].innerText.split(' ');
-    const storeTitleLength = storeTitleSplit.length;
-    const lastWord = storeTitleSplit[storeTitleLength - 1];
-    console.log(lastWord);
+    // const storeTitleSplit = $storeTitleEl[i].innerText.split(' ');
+    console.log($storeTitleEl.html().replace('DISPENSARY', 'DATSPENSARY'));
+    // const storeTitleLength = storeTitleSplit.length;
+    // const lastWord = storeTitleSplit[storeTitleLength - 1];
+    // const lastWordBlock = `<span class="line-break">${lastWord}</span>`
   }
 };
 
