8cf7beef2ee3d11cdc842d923aff132827522d54
georgezikos <george.zikos@icloud.com>
2021-09-26 13:10:25 -0400

    Step by step... oooo baby

--- src/components/stores/storeTitles.js
+++ src/components/stores/storeTitles.js
@@ -2,7 +2,7 @@ const $storeTitle = $('h2.store-locations__heading');
 
 const storeTitles = () => {
   for (let i = 0; i < $storeTitle.length; i++) {
-    console.log($storeTitle[i].innerText);
+    console.log($storeTitle[i].innerText.split(' '));
   }
 };
 
