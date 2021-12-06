fbe8ebd927a75f6432b22bfb7eaa48ef7a6d0ff2
georgezikos <george.zikos@icloud.com>
2021-12-06 11:55:45 -0500

    Worked as intended

--- src/components/Modals/ShopSegmentation/ShopSegmentation.js
+++ src/components/Modals/ShopSegmentation/ShopSegmentation.js
@@ -293,7 +293,7 @@ const ShopSegmentation = () => {
       main = true;
     } else if (dataVal === 'specials') {
       destination =
-        currentDay !== 0 || currentDay !== 6 || currentDay !== 5 // flip back to ===
+        currentDay === 0 || currentDay === 6 || currentDay === 5
           ? privateCollection
           : specials;
     } else if (dataVal === 'flower') {
